900cdefe3f2e0f466ff643171a44664588135682
jeloercc <mjul064@gmail.com>
2026-02-24 14:15:39 -0600

    Update comments to English

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ fetch("https://api.artic.edu/api/v1/artworks")
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        console.log('Primera obra:', data.data[0].title); 
+        console.log("First artwork title:", data.data[0].title); 
     })
     .catch((error) => {
-        console.error("Error al obtener los datos:", error);
+        console.error("Error fetching data:", error);
     });
